37ac2f58161d8371934206c221bc9fcbd83627d1
Mason Mandernach <mmandernach13@gmail.com>
2025-10-12 18:09:55 -0500

    updated api implementation

--- lockin_pid_app.py
+++ lockin_pid_app.py
@@ -7,9 +7,7 @@ from time import sleep,time
 
 
 # PATH of control_hugo.py file
-import sys
-sys.path.append(r'C:\lia_pid_app\rp_lock-in_pid\resources\remote_control')
-from control_hugo import red_pitaya_control,red_pitaya_app
+from app_source.resources.remote_control.control_hugo import red_pitaya_control,red_pitaya_app
 
 AppName      = 'lock_in+pid'
 host         = 'rp-f09094.local'
@@ -21,8 +19,6 @@ trigger_type = 6   # 6 is external trigger
 
 filename = 'test.npz'
 rp=red_pitaya_app(AppName=AppName,host=host,port=port,filename=filename,password='root')
-osc = red_pitaya_control(cmd='/root/py/osc.py', name='osc', parent=rp)
-osc.load()
 
 # reduce log noise on Windows platform
 import logging
@@ -30,7 +26,6 @@ logging.basicConfig()
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 rp.verbose = False
 
-print(osc.get_data())
 # print(rp)
 # print(rp.lock)
 # print(rp.osc)
