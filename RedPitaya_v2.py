d6394068d169219d7b483f3d7db755710a176b17
Mason Mandernach <mmandernach13@gmail.com>
2025-10-22 22:37:09 -0500

    tried getting pid to work, need different soln for set point/io to get phase lock

--- RedPitaya_v2.py
+++ RedPitaya_v2.py
@@ -3,6 +3,7 @@ Created on 09/25/2025
 
 @author: mason mandernach
 """
+import math
 
 from pyrpl import Pyrpl
 import numpy as np
@@ -44,36 +45,57 @@ class RedPitaya:
         self.scope.average = 'true'
         self.sample_rate = 125e6/self.scope.decimation
 
-    def setup_lockin(self, output_ref=False, **params):
+    def setup_lockin(self, params):
         self.ref_freq = params['ref_freq']
+        self.ref_period = 1/self.ref_freq
         ref_amp = params['ref_amp']
 
         self.ref_sig.setup(waveform='sin',
                            amplitude=ref_amp,
                            frequency=self.ref_freq)
 
-        self.ref_start_t = time.time_ns()
+        self.ref_start_t = time.time()
 
-        if hasattr(params, 'output_ref'):
+        if params['output_ref'] == 'out1' or params['output_ref'] == 'out2':
             self.ref_sig.output_direct = params['output_ref']
+        else:
+            self.ref_sig.output_direct = 'off'
 
         self.lockin.setup(frequency=self.ref_freq,
                        bandwidth=[-self.ref_freq * 2, -self.ref_freq, self.ref_freq, self.ref_freq * 2],  # Hz
                        gain=1.0,
-                       phase=(time.time_ns() - self.ref_start_t)*self.ref_freq*1e9*360,       #initial phase is in degrees (delta t[ns])-> delta t [s]/(1/f) * 360
+                       phase=((time.time() - self.ref_start_t)/self.ref_period)*360,       #initial phase is in degrees (delta t[ns])-> delta t [s]/(1/f) * 360
                        acbandwidth=0,
                        amplitude=ref_amp,
                        input='in1',
-                       output_direct='out2',
-                       output_signal='quadrature',
-                       quadrature_factor=20)
+                       output_direct='off',
+                       output_signal='output_direct',
+                       quadrature_factor=1)
 
-    def setup_pid(self, **params):
-        self.pid.input = params['pid_in']
+    def setup_pid(self, params):
+        self.pid.input = 'iq2'
+        self.pid.output_direct = 'out1'
         self.pid.setpoint = params['setpoint']
         self.kp = params['kp']
         self.ki = params['ki']
         self.ival = 0
+
+    def get_ref_phase(self):
+        delta_t = (time.time() - self.ref_start_t)
+        num_periods = delta_t/(1/self.ref_freq)
+        partial = num_periods % 1.0
+        return partial
+
+    def get_pid_output(self):
+        self.scope.input1 = self.pid.output_direct
+        output = np.array(self.scope._data_ch1_current)
+        self.scope.input1 = 'iq2'
+        return output[-1] / 360
+
+    def get_current_lia_phase(self):
+        last_Y = self.lockin_Y[-1]
+        last_X = self.lockin_X[-1]
+        return math.atan(np.mean(last_Y)/np.mean(last_X))
 
     def capture_lockin(self):
         """
@@ -121,28 +143,45 @@ class RedPitaya:
         else:
             plt.show()
 
-    def run(self, params, lock=True, save_file=False, fft=True):
+    def run(self, params):
         timeout = run_params['timeout']
         loop_f = run_params['loop_f']
 
         self.setup_lockin(params)
 
-        if lock:
+        if params['lock']:
             self.setup_pid(params)
         time.sleep(0.01)
 
-        if fft:
-            self.see_fft(save_file=save_file)
+
+        loop_start = time.time()
+        ref_theta = []
+        lia_theta = []
+
+        while (time.time() - loop_start) < timeout:
+            self.capture_lockin()
+            ref_theta.append(self.get_ref_phase())
+            lia_theta.append(self.get_current_lia_phase())
+            # self.lockin.phase = self.get_pid_output()
+            time.sleep(1/loop_f)
+
+        if params['fft']:
+            self.see_fft(save_file=params['save_file'])
         else:
-            X, Y = self.capture_lockin()
+            X, Y = np.concatenate(self.lockin_X), np.concatenate(self.lockin_Y)
             t = np.arange(start=0, stop=len(X)/self.sample_rate, step=1/self.sample_rate)
             plt.plot(t, X)
             plt.plot(t, Y)
             plt.title('Lockin Results')
             plt.xlabel('Time (s)')
             plt.ylabel('X and Y outputs')
+            plt.legend(['X', 'Y'])
+            # plt.plot(ref_theta)
+            # plt.plot(lia_theta)
+            # plt.legend(['ref_theta', 'lia_theta'])
+            # plt.grid(True)
 
-            if save_file:
+            if params['save_file']:
                 if not os.path.exists(self.output_dir):
                     os.makedirs(self.output_dir)
 
@@ -156,15 +195,29 @@ if __name__ == '__main__':
 
     rp = RedPitaya()
 
-    run_params= {
-        'test_freq': 100,
-        'test_amp': 0.4,
-        'noise_freq': 10000,
-        'noise_amp': 0.1,
-        'ref_freq': 100,
-        'ref_amp': 0.4
+    run_params = {
+        'ref_freq': 100,       # Hz, reference signal frequency for lock-in
+        'ref_amp': 0.4,        # V, amplitude of reference signal
+        'output_ref': '',  # where to output the ref_signal
+
+        'setpoint': 0.0,    # target setpoint for PID
+        'kp': 1.0,          # proportional gain
+        'ki': 0.1,          # integral gain
+
+        'lock': True,       # whether or not to phase lock
+        'timeout': 5.0,     # seconds, how long to run acquisition loop
+        'loop_f': rp.sample_rate/10,       # Hz, how often to capture data in run loop
+
+        'test_freq': 100,           # Hz, used for test or calibration runs
+        'test_amp': 0.4,            # V, amplitude of test waveform
+        'noise_freq': 10000,        # Hz, optional noise injection frequency
+        'noise_amp': 0.1,           # V, optional noise amplitude
+
+        'output_dir': 'test_data',  # where to save FFT and waveform plots
+        'save_file': False,         # whether to save plots instead of showing them
+        'fft': False,               # whether to perform FFT after run
     }
 
-    rp.run(run_params, save_file=False)
+    rp.run(run_params)
 
 
--- lia_wave_test.py
+++ lia_wave_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pyrpl import Pyrpl
 
-r = Pyrpl(config='liatest.yaml', hostname='169.254.131.37')
+r = Pyrpl(config='', hostname='169.254.131.37')
 rp = r.rp
 
 pid = rp.pid0
